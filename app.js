ff841e4b33477e8f846e5451d7e7de8a4f766dfb
ɐʇɯon3ɹ <contact.atmoner@gmail.com>
2022-08-31 18:02:32 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+import { assets, chains } from 'chain-registry'
+import fetch from 'node-fetch'
+import fs from 'fs'
+
+var finalData = []
+
+  async function getLcd(chain) {
+    let finder = chains.find(
+      ({chain_name}) => chain_name === chain
+    ) 
+    let finalLcd = ''
+    
+    if (typeof finder !== 'undefined' && typeof finder.apis !== 'undefined') {      
+      for (const lcds of finder.apis.rest) {      
+        try {
+          const response = await fetch(lcds.address + '/node_info')
+          if (response.status == 200) {
+            finalLcd = lcds.address
+            break
+          } 
+        } catch (err) {
+          console.error(err)
+        }        
+      }      
+    }
+    return finalLcd
+  }
+  async function getRpc(chain) {
+    let finder = chains.find(
+      ({chain_name}) => chain_name === chain
+    ) 
+    let finalRpc = ''
+    
+    if (typeof finder !== 'undefined' && typeof finder.apis !== 'undefined') {   
+      for (const rpcs of finder.apis.rpc) {      
+        try {
+          const response = await fetch(rpcs.address)
+          if (response.status == 200) {
+            finalRpc = rpcs.address
+            break
+          } 
+        } catch (err) {
+          console.error(err)
+        }        
+      }      
+    }
+    return finalRpc
+  }
+
+
+var content = ''
+
+  var chainsToCheck = ['cosmoshub', 'bitcanna', 'osmosis', 'desmos', 'chihuahua', 'juno', 'stargaze', 'akash']
+  chainsToCheck.forEach(async function(item) {  
+    
+const assetList = assets.find(({chain_name})=>chain_name===item);
+
+console.log(assetList.assets[0].logo_URIs.png);    
+    
+    var lcd = await getLcd(item)
+    var rpc = await getRpc(item)
+    console.log(lcd, rpc)
+    content += `### <img alt="`+item+`" src="`+assetList.assets[0].logo_URIs.png+`" width="30" height="30"> ` + item + `
+`
+    content += `&emsp; LCD :green_circle: ` + lcd + `  
+`
+    content += `&emsp; RPC :green_circle: ` + rpc + `  
+`
+    fs.writeFileSync("README.md", content);
+     
+  })  
+  
+ 
+ 
